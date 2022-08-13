d9099c2e13dc4f54fee215d0f8819e57a6f3a9fe
Asmaa <asmaakhdr24@gmail.com>
2022-08-13 09:33:16 +0200

    Add layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 import React from 'react';
-
+import styles from '../styles/Navbar.module.css';
 const Navbar = () => {
   return (
-    <div>
+    <div className={styles.bg}>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/posts/1">Post1</Link>
