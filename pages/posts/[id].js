76a6d9af31a2b2d32673a24114be35b6afe4b381
Asmaa <asmaakhdr24@gmail.com>
2022-08-13 13:31:54 +0200

    post details

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -10,7 +10,22 @@ const Post1 = (props) => {
   );
 };
 
-export async function getServerSideProps(context) {
+export async function getStaticPaths() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+  const data = await res.json();
+
+  const paths = data.map((d) => {
+    return {
+      params: { id: `${d.id}` },
+    };
+  });
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
   const data = await res.json();
 
