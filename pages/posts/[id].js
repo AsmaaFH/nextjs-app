384fdc124d3493f3b5c09703ff6386596298abf2
Asmaa <asmaakhdr24@gmail.com>
2022-08-13 10:51:33 +0200

    getServerSideProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const Post1 = () => {
-  return <div>Post1</div>;
+const Post1 = (props) => {
+  console.log(props);
+  return (
+    <>
+      <h4>{props.post.title}</h4>
+      <p>{props.post.body}</p>
+    </>
+  );
 };
+
+export async function getServerSideProps(context) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+  const data = await res.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
 
 export default Post1;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 
 const posts = (props) => {
+  console.log(props.posts);
   return (
     <>
       <h1>Posts</h1>
       {props.posts.map((post) => (
-        <div key={post.id}>{post.title}</div>
+        <Link key={post.id} href={`/posts/${post.id}`}>
+          {post.title}
+        </Link>
       ))}
     </>
   );
